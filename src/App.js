7bfa5200d06427361ae04a84d33bb76e6aece01b
dsi28 <dsi28@hotmail.com>
2024-07-26 12:31:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div><div>Hello {value}</div> <a href="/.auth/login/aad">Login</a></div>;
 }
 
 export default App;
